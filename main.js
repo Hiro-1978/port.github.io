091a76c7b43bdfaf32aa8d571b09a80f71abe8f8
Hiro-1978 <hiromiuki11@gmail.com>
2021-05-13 14:21:31 +0700

    Send Email
    send mail

--- main.js
+++ main.js
@@ -5,6 +5,20 @@ const menuToggle = document.querySelector('.hamburger-menu');
 const hiroToggle = document.querySelector('.hiro');
 const navlist = document.querySelector('.nav-list');
 
+function sendEmail(params) {
+    let tempParams ={
+        from_name:document.getElementById("fromName").Value,
+        to_name:document.getElementById("name").Value,
+        message:document.getElementById("message").Value,
+    };
+
+    emailjs.send('gmail','template_rzz6m3f',tempParams)
+    .then(function(res){
+        console.log("ส่งข้อความไปที่เมลล์แล้ว", res.status);
+    })
+}
+
+
 /*เวลากดที่เมนูย่อแล้วจะเลื่อนหน้าเมนูออกมาให้ใช้งาน*/
 menuToggle.addEventListener('click', function(){
     bodyElement.classList.toggle('is-open');
